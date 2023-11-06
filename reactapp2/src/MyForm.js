1bef3031172780cbea346af47978c2863457324b
unknown <aliibneimran@gmail.com>
2023-11-06 21:43:10 +0600

    add

--- reactapp2/src/MyForm.js
+++ reactapp2/src/MyForm.js
@@ -4,15 +4,22 @@ import React, { useState } from 'react';
 export default function MyForm() {
     const[name,setName] = useState("");
     const[phone,setPhone] = useState("");
+    const[email,setEmail] = useState("");
+    const[address,setAddress] = useState("");
   return (
     <div>
         <h1>Entry Form</h1>
         <form action="">
-          Name : <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/><br/><br/>
-          Phone : <input type="number" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
+          <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Your Name'/><br/><br/>
+          <input type="number" value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder='Enter Your Phone'/><br/><br/>
+          <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Your Email'/><br/><br/>
+          <textarea value={address} onChange={(e)=>setAddress(e.target.value)} placeholder='Enter Your Address'></textarea><br/><br/>
+          <button>Submit</button>
         </form><br/>
         Name : {name}<br/>
-        Phone : {phone}
+        Phone : {phone}<br/>
+        Email : {email}<br/>
+        Address : {address}
     </div>
   )
 }
