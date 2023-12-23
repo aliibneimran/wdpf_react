25eb81a390ba1f34929a4671fdfbc80d3c90a4f6
unknown <aliibneimran@gmail.com>
2023-12-23 23:22:25 +0600

    add

--- real_estate/api/property.php
+++ real_estate/api/property.php
@@ -1,14 +1,14 @@
 <?php
 require 'db_connection.php';
-	$allProperty = $db->query("SELECT * FROM property JOIN types ON property.type_id = types.t_id");
+	$allProperty = $db->query("SELECT * FROM property JOIN types ON property.type_id = types.t_id JOIN categories ON property.category_id = categories.c_id JOIN statuses ON property.status_id = statuses.s_id");
 		if(mysqli_num_rows($allProperty) > 0){
 			while($row = $allProperty->fetch_array()){
 				$viewjson["id"] = $row['id'];
 				$viewjson["title"] = $row['title'];
 				$viewjson["description"] = $row['description'];
-				$viewjson["category_id"] = $row['category_id'];
+				$viewjson["category"] = $row['category'];
 				$viewjson["type"] = $row['type'];
-				$viewjson["status_id"] = $row['status_id'];
+				$viewjson["status"] = $row['status'];
 				$viewjson["size"] = $row['size'];
 				$viewjson["bed"] = $row['bed'];
 				$viewjson["bath"] = $row['bath'];
